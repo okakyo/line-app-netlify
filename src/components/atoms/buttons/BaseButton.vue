9269e70f4b3483696cf63f7e28cd95aa485559de
=okakyo <=0622okakyo@gmail.com>
2020-09-27 12:23:28 +0900

    Function部分の動作確認

---
+++ src/components/atoms/buttons/BaseButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <button></button>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+type ButtonProps = {
+  large: boolean;
+  small: boolean;
+  block: boolean;
+  rounded: boolean;
+  outlined: boolean;
+  text: boolean;
+  color: string;
+};
+
+export default defineComponent({
+  name: "BaseButton",
+  props: {
+    large: Boolean,
+    small: Boolean,
+    block: Boolean,
+    rounded: Boolean,
+    outlined: Boolean,
+    text: Boolean,
+    color: String
+  },
+  setup(props: any) {
+    const classes = () => {
+      return {
+        large: props.large,
+        small: props.small,
+        block: props.block,
+        rounded: props.rounded,
+        outlined: props.outlined
+      };
+    };
+  }
+});
+</script>
