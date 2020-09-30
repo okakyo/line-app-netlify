b0f898779951eccf14629d51b7642f2bba1d587c
=okakyo <=0622okakyo@gmail.com>
2020-10-01 00:19:12 +0900

    Functions のエラーを修正

--- src/lambda/api.ts
+++ src/lambda/api.ts
@@ -5,13 +5,11 @@ import serverless from "serverless-http";
 import {baseController} from "./controller/index";
 const app = express();
 
-const basicRouter = process.env.NODE_ENV ==="production"?"/.netlify/functions/api":"/api"
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
-console.log(process.env.NODE_ENV);
-console.log("ルーター",basicRouter);
-app.use("/api",baseController)
+app.use("/.netlify/functions/api",baseController)
 
 export const handler = serverless(app);
