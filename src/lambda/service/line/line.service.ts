44425ab85ab9c475d2b4fabce4a54c822b68fccc
=okakyo <=0622okakyo@gmail.com>
2020-10-03 14:34:04 +0900

    ボットの仕様を追加

--- src/lambda/service/line/line.service.ts
+++ src/lambda/service/line/line.service.ts
@@ -2,26 +2,43 @@
 import {Client} from "@line/bot-sdk";
 
 // handle the text message function.
-const TextHandler = (event:any) =>{
-    return {};
+const TextHandler = (event:any,client:Client) =>{
+  return client.replyMessage(event.replyToken,{
+    type: 'text',
+    text: event.message.text
+});
 }
 // handle the image message function.
-const imgHandler = (event:any)=>{
-    return {};
+const imgHandler = (event:any,client:Client)=>{
+    return client.replyMessage(event.replyToken,{
+        type: 'text',
+        text: "こちらの設定については未設定です。"
+    });
 }
 
 // ここで Service Function としてexport する
 //TODO: Client をDI 化したほうがいい
 
 export const handleEvent = (event:any,client:Client) =>{
+    
     if (event.type !== 'message' || event.message.type !== 'text') {
       return Promise.resolve(null);
     }
-
-    return client.replyMessage(event.replyToken,{
-      type: 'text',
-      text: event.message.text
+    switch(event.message.type){
+      case "text":
+        return TextHandler(event,client);
+      case "image":
+        return imgHandler(event,client);
+      case "location":
+        return;
+      default:
+        return client.replyMessage(event.replyToken,{
+            type: 'text',
+            text: "こちらの設定については未設定です。"
     });
+        break
+    }
+    
 }
 
 
--- src/lambda/service/line/lineTemplate.ts
+++ src/lambda/service/line/lineTemplate.ts
@@ -1,18 +1,72 @@
-import { TemplateCarousel, TemplateConfirm, TemplateMessage } from '@line/bot-sdk';
+import { Action, FlexBox, FlexBubble, FlexBubbleStyle, FlexCarousel, FlexImage, TemplateButtons, TemplateConfirm} from '@line/bot-sdk';
+import bodyParser from 'body-parser';
 
-// TODO: カードテンプレートを設定する
-export const CardTemplate:TemplateMessage = ()=>{
-    return {}
-};
+
 
 // TODO: カルーセルテンプレートを設定する
-export const caroucelTemplate:TemplateCarousel =()=>{
-    return {}
+export const caroucelTemplate =(content:FlexBubble[]):FlexCarousel=>{
+    return {
+        type:"carousel",
+        contents:content
+    }
 }
+
+// TODO: カードテンプレートを設定する
+export const CardTemplate= (
+    size?:"nano" | "micro" | "kilo" | "mega" | "giga",
+    direction?:"ltr" | "rtl",
+    header?: FlexBox,
+    hero?: FlexBox | FlexImage,
+    body?: FlexBox,
+    footer?: FlexBox,
+    styles?: FlexBubbleStyle,
+    
+    ):FlexBubble =>{
+   return {
+       type:"bubble",
+       size:size,
+       direction:direction,
+       header:header,
+       hero:hero,
+       body:body,
+       footer:footer,
+       styles:styles,
+
+
+       
+   }
+};
 
 // TODO: コンファームボタンのテンプレートを設定する
-export const ConfirmTemplate:TemplateConfirm =()=>{
-    return {}
+export const ConfirmTemplate =(text:string, actions:Action[]):TemplateConfirm=>{
+    return {
+        type:"confirm",
+        text:text,
+        actions:actions
+    }
 }
 
+//TODO:  Button テンプレートを設定する
+export const ButtonTemplate = (
+
+    text: string, 
+    actions:Action[],
+    thumbbnail?:string,
+    title?:string,
+    imageRatio?:"rectangle"|"square",
+    imageBgColor?:string,
+
+    ):TemplateButtons =>{
+
+    return {
+        type:"buttons",
+        title:title,
+        thumbnailImageUrl:thumbbnail,
+        text:text,
+        imageAspectRatio:imageRatio,
+        imageBackgroundColor:imageBgColor,
+        actions:actions
+    }
+    
+}
 
